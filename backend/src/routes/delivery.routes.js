54f916e08725f4aeee65b6f1513bf78e34466113
lakhrouf mohamed <mohamedlakhrouf@gmail.com>
2026-01-08 22:17:11 +0100

    added delivery controller

--- backend/src/routes/delivery.routes.js
+++ backend/src/routes/delivery.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createDelivery } from "../controllers/delivery.controller.js";
+import createDelivery from "../controllers/delivery.controller.js";
 
 const router = express.Router();
 
 router.post("/", createDelivery);
 
 export default router;
+ 
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,10 +4,8 @@ import deliveryRoutes from "./delivery.routes.js";
 
 const router = Router();
 
-// Health
 router.use("/", healthRoutes);
 
-// Deliveries
 router.use("/deliveries", deliveryRoutes);
 
 export default router;
