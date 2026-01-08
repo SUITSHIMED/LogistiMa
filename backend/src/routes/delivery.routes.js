43f02d9622453822cd001b8a2f908e20d5cf7c02
lakhrouf mohamed <mohamedlakhrouf@gmail.com>
2026-01-08 22:24:11 +0100

    fix delivery route file

--- backend/src/routes/delivery.routes.js
+++ backend/src/routes/delivery.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import createDelivery from "../controllers/delivery.controller.js";
+import { 
+  createDelivery,
+  getAllDeliveries,
+  getDeliveryById
+} from "../controllers/delivery.controller.js"; 
 
 const router = express.Router();
 
 router.post("/", createDelivery);
+router.get("/", getAllDeliveries);
+router.get("/:id", getDeliveryById);
 
 export default router;
- 
